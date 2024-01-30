3be6b0b82e2b8a295b5f2f6ebb880f3c5bc69ec6
Akshansh <s.akshansh01@gmail.com>
2024-01-31 02:28:57 +0530

    Linked frontend and backend

--- Todo-app react+node/frontend/src/components/CreateTodo.jsx
+++ Todo-app react+node/frontend/src/components/CreateTodo.jsx
@@ -1,36 +1,31 @@
 import { useState } from "react";
 
+export function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-export function CreateTodo(){
+  const addTodo = () => {
+    fetch("http://localhost:3000/todo", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description
+      })
+    })
+    .then(async function(res){
+      const json = await res.json();
+      alert("Todo added");
+    })
+  }
 
-    const [title,setTitle]=useState("");
-    const [description,setDescription]=useState("");
-
-    return <div>
-        <input id="title" style= {{
-            padding:10,
-            margin:10
-        }} type="text" placeholder="title"></input><br></br>
-        <input id="description" style= {{
-            padding:10,
-            margin:10
-        }} type="text" placeholder="description"></input><br></br>
-
-        <button style= {{
-            padding:10,
-            margin:10
-        }} onClick={()=>{
-            fetch("http://localhost:3000/todos"), {
-                method: "POST",
-                body: {
-                    title: title,
-                    description: description
-                }
-            }
-            .then(async function(res){
-                const json = await res.json();
-                alert("Todo added");
-            })
-        }}>Add a todo</button>
+  return (
+    <div>
+      <input id="title" style={{ padding:10, margin:10 }} type="text" placeholder="title" onChange={e => setTitle(e.target.value)}></input><br></br>
+      <input id="description" style={{ padding:10, margin:10 }} type="text" placeholder="description" onChange={e => setDescription(e.target.value)}></input><br></br>
+      <button style={{ padding:10, margin:10 }} onClick={addTodo}>Add a todo</button>
     </div>
+  )
 }
