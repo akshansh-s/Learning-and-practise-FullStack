44bcd49454fb4e8c49191bb1ebdb85f17ee6ae53
Akshansh <s.akshansh01@gmail.com>
2024-02-09 23:11:08 +0530

    added button functionality

--- Todo fetching from server/Todo/src/App.jsx
+++ Todo fetching from server/Todo/src/App.jsx
@@ -3,8 +3,19 @@ import axios from "axios";
 import './App.css'
 
 function App() {
+  const [idi, setIdi] = useState(1);
   return <div>
-    <Todo id={3}/>
+    <button onClick={function (){
+      setIdi(1);
+    }}>1</button>
+    <button onClick={function (){
+      setIdi(2);
+    }}>2</button>
+    <button onClick={() => setIdi(3)}>3</button>
+    <button onClick={() => setIdi(4)}>4</button>
+    <button onClick={() => setIdi(5)}>5</button>
+
+    <Todo id={idi}/>
   </div>
   
 }
@@ -17,7 +28,7 @@ function Todo({id}) {
       .then(response=>{
         setTodo(response.data.todo)
       })
-  }, []);
+  }, [id]);
   // return (
   //   <>
   //     {todos.map(todo=> <Todo title={todo.title} description= {todo.description} />)}
