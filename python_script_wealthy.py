36135b2fec6fff47322c0672732d4292fe9c0d66
Akshansh <s.akshansh01@gmail.com>
2024-04-06 20:08:54 +0530

    changes in script

--- python_script_wealthy.py
+++ python_script_wealthy.py
@@ -3,9 +3,10 @@ import json
 
 def fetch_data(wschemecodes):
     URL = 'https://fundsapi.wealthy.in/api/v2/mf-funds/'
-    HEADER = {'Protected'}
+    HEADER = {'protected'}
     test=0
     results = []  
+    missing_isin_list=[]
     
     for code in wschemecodes:
         params = {'wschemecodes': code}
@@ -32,30 +33,31 @@ def fetch_data(wschemecodes):
                 else:
                     year_trail = 0.2
                 display_name = item.get("display_name") or item.get("scheme_name")
-                
+                isin_code= item.get("isin_code") or item.get("isin_reinvestment")
                 required_data = {
-                    "display_name": display_name,
-                    "fund_type": item["fund_type"],
-                    "isin_code": item["isin_code"],
-                    "effective_from": "2000-01-01",  
-                    "effective_to": "2024-03-31",
-                    "wschemecode": item["wschemecode"],
-                    "isin_code": item["isin_code"], 
-                    "wpc":item["wpc"],
-                    "b30_trail": 0 ,
-                    "year_trail": year_trail
-                }
-                
-                    
-                results.append(required_data)
+                        "display_name": display_name,
+                        "fund_type": item["fund_type"],
+                        "effective_from": "2000-01-01",  
+                        "effective_to": "2024-03-31",
+                        "wschemecode": item["wschemecode"],
+                        "isin_code": isin_code, 
+                        "wpc":item["wpc"],
+                        "b30_trail": 0 ,
+                        "year_trail": year_trail
+                    }
+                if isin_code:
+                    #results.append(required_data)
+                    print(isin_code)
+                else:
+                    missing_isin_list.append(required_data)
         else:
             print(f"Failed to fetch data for {code}. Status code: {response.status_code}")
     
-    return results
+    return results, missing_isin_list
 
 
 wschemecodes=['IN966L01CQ2','MSBIL121DGO','INF179KC1ID2', 'INF082J01465', 'INF090I01197', 'INF090I01221','INF192K01932', 'INF090I01536''INF090I01866', 'INF090I01940', 'INF090I01973', 'INF090I01AA5', 'INF0K1H01115', 'INF109K01JK9']
-fetched_data = fetch_data(wschemecodes)
+fetched_data, missing_isin_list = fetch_data(wschemecodes)
 
 json_data = json.dumps(fetched_data, indent=4)
 #print(json_data)
@@ -65,6 +67,10 @@ file = "/Users/wealthy/Desktop/fetched_data_final.json"
 with open(file, 'w') as file:
     file.write(json_data)
 
+missing_isin_data = json.dumps(missing_isin_list, indent=4)
+missing_isin_file_path = "/Users/wealthy/Desktop/missing_isin_data_final.json"
+with open(missing_isin_file_path, 'w') as file:
+    file.write(missing_isin_data)
+
 print(f"Data saved to {file}")
-
-
+print(f"List of items missing ISIN codes saved to {missing_isin_file_path}")
